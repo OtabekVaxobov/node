6eb061fb04a33308694f3d2e0e2c10237b875c81
OtabekVaxobov <elot2017@mail.ru>
2021-02-16 14:52:02 +0300

    add gulpfile

---
+++ gulp-example/gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+
+gulp.task('default', () => {
+    return gulp.src('app/*.jsx')
+    .pipe(sourcemaps.init()) //Запускает анализ файлов для построения отладочных карт исходного кода
+    .pipe(babel({
+        presets: ['es2015', 'react'] // Настраивает gulp-babel для использования ES2015 и React
+    }))
+    .pipe(concat('all.js')) //Объединяет все файлы с исходным кодом в all.js
+    .pipe(sourcemaps.write('.')) //Записывает файлы с картами отдельно.
+    .pipe(gulp.dest('dest')); //Перенаправляет все файлы в dist/folder.
+});
