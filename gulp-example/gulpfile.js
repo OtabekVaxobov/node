7a94cbbea3ea136810d74e64aebb7a9405218bbb
OtabekVaxobov <elot2017@mail.ru>
2021-02-16 18:44:53 +0300

    add gulpfile

--- gulp-example/gulpfile.js
+++ gulp-example/gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
+const watch = require('gulp-watch');
 
 gulp.task('default', () => {
     return gulp.src('app/*.jsx')
@@ -12,4 +13,8 @@ gulp.task('default', () => {
     .pipe(concat('all.js')) //Объединяет все файлы с исходным кодом в all.js
     .pipe(sourcemaps.write('.')) //Записывает файлы с картами отдельно.
     .pipe(gulp.dest('dest')); //Перенаправляет все файлы в dist/folder.
+});
+
+gulp.task('watch', () => {
+    watch('app/**.jsx', () => gulp.start('default'));
 });
